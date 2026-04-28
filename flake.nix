353bf5a56dfcc9eea747840e929e33a346b6faca
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-04-28 13:32:30 +0900

    :snowflake: Introduce Nix flake with devShell and pre-commit hooks
    Provides a reproducible development environment for Europa.vim using
    flake-parts, including deno/pandoc/nodejs and pre-commit hooks
    (nixfmt, typos, end-of-file-fixer, deno fmt, deno lint).

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Europa.vim";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
+
+        inputs.git-hooks.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          pre-commit.settings.hooks = {
+            # Nix formatter
+            nixfmt.enable = true;
+
+            # General hygiene
+            typos.enable = true;
+            end-of-file-fixer.enable = true;
+
+            deno-fmt = {
+              enable = true;
+              name = "deno fmt --check";
+              entry = "${pkgs.deno}/bin/deno fmt --check";
+              files = "\\.(ts|js|tsx|jsx|json|jsonc)$";
+              pass_filenames = false;
+            };
+
+            deno-lint = {
+              enable = true;
+              name = "deno lint";
+              entry = "${pkgs.deno}/bin/deno lint";
+              files = "\\.(ts|js|tsx|jsx)$";
+              pass_filenames = false;
+            };
+          };
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ config.pre-commit.devShell ];
+            packages = with pkgs; [
+              deno # runtime + fmt + lint + test + runs npm:typedoc
+              pandoc # required by panvimdoc (final stage: md -> vimdoc)
+              nodejs # for npm: packages with native deps
+
+              typos
+
+              # Added in Phase 1 (when implementing Sixel image rendering)
+              # imagemagick     # used for PNG -> Sixel conversion
+            ];
+          };
+
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
